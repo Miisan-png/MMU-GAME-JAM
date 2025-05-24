d1afa4a436640c6ca3c22c400d9e11ee436effe4
Miisane <azarmisan@gmail.com>
2025-05-25 07:08:10 +0800

    add: puzzle system in

---
+++ test/core.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+@onready var animation_player: AnimationPlayer = $FadeIn/AnimationPlayer
+@onready var fade_in: Control = $FadeIn
+
+func _ready() -> void:
+	animation_player.play("fade_in")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fade_in":
+		fade_in.queue_free()
