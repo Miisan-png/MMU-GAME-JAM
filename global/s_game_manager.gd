f6df67f082b58567290a1ba4eeccc6c6d53ae7c0
StarryStuff <starrystufff@gmail.com>
2025-05-25 08:36:20 +0800

    Gameplay puzzles added

--- global/s_game_manager.gd
+++ global/s_game_manager.gd
@@ -4,7 +4,7 @@ var whistle_mode_enabled:bool = false
 
 
 #puzzle managers
-var p_keycard_collected:bool = false
+var p_keycard_collected:bool = true
 var i_walkie_picked_up:bool = false
 var keypad_true:bool = false
 var in_keypad_state:bool = false
